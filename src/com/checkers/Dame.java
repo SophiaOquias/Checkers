35d6f75cf9ca4dc361556bc7f5376d4640394d18
Sophia <sophia.oquias@gmail.com>
2021-12-14 19:57:42 +0800

    created Dame class and added getters and setters

--- src/com/checkers/Dame.java
+++ src/com/checkers/Dame.java
@@ -1,2 +1,44 @@
-package com.checkers;public class Dame {
+package com.checkers;
+
+public class Dame {
+    // PROPERTIES
+    protected Position pos;
+    protected Color color;
+
+    // CONSTRUCTORS
+    public Dame(Position pos, Color color) {
+        this.pos = pos;
+        this.color = color;
+    }
+
+    public Dame(int row, int col, Color color) {
+        this.pos = new Position(row, col);
+        this.color = color;
+    }
+
+    // METHODS
+
+    public Position getPos() {
+        return pos;
+    }
+
+    public void setPos(Position pos) {
+        this.pos = pos;
+    }
+
+    public void setPos(int row, int col) {
+        this.pos = new Position(row, col);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    // public void move
+
+    // public void capture 
 }
