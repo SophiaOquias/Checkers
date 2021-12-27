d1cd35ac62083acd479e730e93ec6c450e7c06ba
Sophia <sophia.oquias@gmail.com>
2021-12-27 08:39:54 +0800

    added methods

--- src/com/checkers/Board.java
+++ src/com/checkers/Board.java
@@ -4,20 +4,128 @@ public class Board {
 
     // PROPERTIES
     protected Dame[][] dames;
-    private final int SIZE = 8;
+    protected final int SIZE = 8;
 
     // CONSTRUCTOR
-    public Board(Board board) {
-
-    }
-
     public Board() {
+        this.dames = new Dame[SIZE][SIZE];
+
         // ADD DAMES
         // white dames
         for(int i = 0; i < 3; i++) {
-            
+            for(int j = 0; j < SIZE; j++) {
+                if(i % 2 == 0 && j % 2 != 0) {
+                    this.dames[i][j] = new Dame(i, j, Color.WHITE);
+                }
+                else if(i % 2 != 0 && j % 2 == 0) {
+                    this.dames[i][j] = new Dame(i, j, Color.WHITE);
+                }
+                else {
+                    this.dames[i][j] = null;
+                }
+            }
+        }
+
+        // black dames
+        for(int i = 5; i < SIZE; i++) {
+            for(int j = 0; j < SIZE; j++) {
+                if(i % 2 != 0 && j % 2 == 0) {
+                    this.dames[i][j] = new Dame(i, j, Color.BLACK);
+                }
+                else if(i % 2 == 0 && j % 2 != 0) {
+                    this.dames[i][j] = new Dame(i, j, Color.BLACK);
+                }
+                else {
+                    this.dames[i][j] = null;
+                }
+            }
         }
     }
 
+    // copy constructor
+    public Board(Board board) {
+        this.copy(board);
+    }
+
     // METHODS
+    public void copy(Board board) {
+        for(int i = 0; i < SIZE; i++) {
+            for(int j = 0; j < SIZE; j++) {
+                if(board.getDame(i, j) != null) {
+                    if(board.getDame(i, j).getColor() == Color.WHITE) {
+                        this.dames[i][j] = new Dame(i, j, Color.WHITE);
+                    }
+                    else {
+                        this.dames[i][j] = new Dame(i, j, Color.BLACK);
+                    }
+                }
+                else {
+                    this.dames[i][j] = null;
+                }
+            }
+        }
+    }
+    public int countWhite() {
+        int count = 0;
+
+        for(int i = 0; i < SIZE; i++) {
+            for(int j = 0; j < SIZE; j++) {
+                if(this.dames[i][j].getColor() == Color.WHITE)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    public int countBlack() {
+        int count = 0;
+
+        for(int i = 0; i < SIZE; i++) {
+            for(int j = 0; j < SIZE; j++) {
+                if(this.dames[i][j].getColor() == Color.BLACK)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    public boolean isSquareOccupied(int row, int col) {
+        if(this.dames[row][col] != null)
+            return true;
+
+        return false;
+    }
+
+    public boolean isSquareOccupied(Position pos) {
+        if(this.dames[pos.getRow()][pos.getCol()] != null)
+            return true;
+
+        return false;
+    }
+
+    public Dame getDame(int row, int col) {
+        return this.dames[row][col];
+    }
+
+    public Dame getDame(Position pos) {
+        return this.dames[pos.getRow()][pos.getCol()];
+    }
+
+    public Dame[][] getDames() {
+        return dames;
+    }
+
+    public void setDame(int row, int col, Dame dame) {
+        this.dames[row][col] = dame;
+    }
+
+    public void setDame(Position pos, Dame dame) {
+        this.dames[pos.getRow()][pos.getCol()] = dame;
+    }
+
+    public void setDames(Dame[][] dames) {
+        this.dames = dames;
+    }
 }
