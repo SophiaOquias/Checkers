7e452cd29c38c99b8de6ba641d9d73d6e8f3e61c
Sophia <sophia.oquias@gmail.com>
2021-12-27 08:52:15 +0800

    created capture and move methods

--- src/com/checkers/Dame.java
+++ src/com/checkers/Dame.java
@@ -17,9 +17,16 @@ public class Dame {
     }
 
     // METHODS
-
     public Position getPos() {
         return pos;
+    }
+
+    public int getRow() {
+        return this.pos.getRow();
+    }
+
+    public int getCol() {
+        return this.pos.getCol();
     }
 
     public void setPos(Position pos) {
@@ -39,8 +46,13 @@ public class Dame {
     }
 
     public void move(Position pos, Board board) {
-
+        board.setDame(pos.getRow(), pos.getCol(), this); // move dame to new pos
+        board.setDame(this.getRow(), this.getCol(), null); // set old pos to null
+        this.pos = pos; // set new position of dame
     }
 
-    // public void capture
+    public void capture(Position pos, Board board, Dame captured) {
+        this.move(pos, board); // move dame to new position
+        board.setDame(captured.getPos(), null); // remove captured dame 
+    }
 }
