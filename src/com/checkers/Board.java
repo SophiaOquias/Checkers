23dcf474b99f3376db33a04672051309866ed507
Sophia <sophia.oquias@gmail.com>
2021-12-27 08:03:14 +0800

    created Board class

---
+++ src/com/checkers/Board.java
@@ -0,0 +1,23 @@
+package com.checkers;
+
+public class Board {
+
+    // PROPERTIES
+    protected Dame[][] dames;
+    private final int SIZE = 8;
+
+    // CONSTRUCTOR
+    public Board(Board board) {
+
+    }
+
+    public Board() {
+        // ADD DAMES
+        // white dames
+        for(int i = 0; i < 3; i++) {
+            
+        }
+    }
+
+    // METHODS
+}
