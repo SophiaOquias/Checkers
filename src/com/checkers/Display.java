ba7d3ba82e5c5c4c876b3052baf82672c87e7a89
Sophia <sophia.oquias@gmail.com>
2021-12-29 12:55:54 +0800

    tested getWhiteChildren successfully

--- src/com/checkers/Display.java
+++ src/com/checkers/Display.java
@@ -30,27 +30,38 @@ public class Display {
 
         d.print(board);
 
-        System.out.println("Is white winner?: " + gm.isWhiteWinner(board));
+        BoardNode node = new BoardNode(board, true);
+        MaxBot bot = new MaxBot();
 
-        // remove all black dames
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 8; j++) {
-                board.setDame(i, j, new Empty());
-            }
-        }
+        node.setChildren(bot.getWhiteChildren(node));
 
-        board.setDame(4, 1, new Dame(4, 1, Color.BLACK));
+        System.out.println("Number of chidlren: " + node.getChildren().size());
 
-        d.print(board);
+//        for(int i = 0; i < node.getChildren().size(); i++) {
+//            System.out.println("i: " + i);
+//            d.print(node.getChildren().get(i));
+//        }
 
-        System.out.println("B Possible moves: " +
-                gm.getPossibleMoves(board.getDame(4, 1), board).length);
-
-        System.out.println("Is white winner?: " + gm.isWhiteWinner(board));
-
-        System.out.println("WHITE: " + board.countWhite() + " BLACK: " + board.countBlack());
-
-        System.out.println("Is there a mandatory move?: " + gm.isMandatory(board));
+//        System.out.println("Is white winner?: " + gm.isWhiteWinner(board));
+//
+//        // remove all black dames
+//        for(int i = 0; i < 3; i++) {
+//            for(int j = 0; j < 8; j++) {
+//                board.setDame(i, j, new Empty());
+//            }
+//        }
+//
+//        board.setDame(4, 1, new Dame(4, 1, Color.BLACK));
+//
+//        d.print(board);
+//
+//        System.out.println("Num Possible moves: " + gm.getNumPossibleMoves(board, Color.WHITE));
+//
+//        System.out.println("Is white winner?: " + gm.isWhiteWinner(board));
+//
+//        System.out.println("WHITE: " + board.countWhite() + " BLACK: " + board.countBlack());
+//
+//        System.out.println("Is there a mandatory move?: " + gm.isMandatory(board));
 
 //        board.getDame(5, 0).move(new Position(4, 1), board);
 //        board.getDame(2, 3).move(new Position(3, 2), board);
