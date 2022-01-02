95b728fc3c520726a576d3b6ebaf2b52ac9a3ef2
Sophia <sophia.oquias@gmail.com>
2022-01-02 09:21:01 +0800

    created Driver

---
+++ src/com/checkers/Driver.java
@@ -0,0 +1,35 @@
+package com.checkers;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Driver extends JFrame {
+
+    private JLabel statusbar;
+
+    public Driver() {
+        statusbar = new JLabel("");
+        this.add(statusbar, BorderLayout.SOUTH);
+
+        this.add(new GameUI(statusbar));
+
+        this.setResizable(false);
+        this.pack();
+
+        this.setTitle("Checkers");
+        var path = "src/images/5.png";
+        Image icon = (new ImageIcon(path)).getImage();
+        this.setIconImage(icon);
+
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+
+            var ex = new Driver();
+            ex.setVisible(true);
+        });
+    }
+}
